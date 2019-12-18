554889343a999bd7a92cb41f976b51e45ff64382
tanHaLiLuYa <tanpeng.hyz@gmail.com>
2019-12-18 14:42:18 +0800

    pygames

--- pythonFile/pygame/ticTacToe.py
+++ pythonFile/pygame/ticTacToe.py
@@ -2,16 +2,17 @@ import random
 
 
 def drawBoard(board):
+    print('-----------')
     print(' | |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print(' '+board[7]+' | '+board[8]+' | '+board[9])
     print('-----------')
     print(' | |')
     print(' | |')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print(' '+board[4]+' | '+board[5]+' | '+board[6])
     print(' | |')
     print('-----------')
     print(' | |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print(' '+board[1]+' | '+board[2]+' | '+board[3])
     print(' | |')
 
 
@@ -130,8 +131,10 @@ def isBoardFull(board):
 
 # 主程序
 print('Welcome to Tic Tac Toe!')
+
 while True:
     # Reset the board
+    print()
     theBoard = [' '] * 10
     playerLetter, computerLetter = inputPlayerLetter()
     turn = whoGoesFirst()
